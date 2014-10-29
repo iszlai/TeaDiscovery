fa13e3f941757d2e598026f233d65ff2948bac0c
Lehel Iszlai <iszlai.lehel@gmail.com>
2014-10-29 21:39:54 +0100

    initial upload

---
+++ src/main/java/A.java
@@ -0,0 +1,6 @@
+package main.java;
+public class A{
+	public void print(){
+		System.out.println("Bye");
+	}
+}
---
+++ src/main/scala/hw.scala
@@ -0,0 +1,11 @@
+
+
+import main.java.A
+
+object B {
+
+  def main(args: Array[String]) {
+    val a = new A()
+    a.print()
+  }
+}
